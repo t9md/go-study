7bb80b48c70828e39c9521e4726e0f056575e099
t9md <taqumd@gmail.com>
2015-02-06 18:34:35 +0900

    upadte

--- README.md
+++
@@ -1,5 +0,0 @@
-# go-learn
-self learning
-
-# reference
-[Go by Example](https://gobyexample.com)
--- recursion.go
+++ recursion.go
@@ -23,12 +23,12 @@ func (i *Indent) UnIndent() {
 
 func fact(n int, i *Indent) int {
 	i.Indent()
-	fmt.Printf("%s %d: called by value\n", i, n)
+	fmt.Printf("%s %d: called\n", i, n)
 	if n == 0 {
 		return 1
 	}
 	answer := fact(n-1, i)
-	fmt.Printf("%s %d: returned from\n", i, n-1)
+	fmt.Printf("%s %d: returned\n", i, n-1)
 	i.UnIndent()
 	return n * answer
 }
@@ -39,20 +39,20 @@ func main() {
 
 /*
 =>
-     7: called by value
-       6: called by value
-         5: called by value
-           4: called by value
-             3: called by value
-               2: called by value
-                 1: called by value
-                   0: called by value
-                   0: returned from
-                 1: returned from
-               2: returned from
-             3: returned from
-           4: returned from
-         5: returned from
-       6: returned from
+     7: called
+       6: called
+         5: called
+           4: called
+             3: called
+               2: called
+                 1: called
+                   0: called
+                   0: returned
+                 1: returned
+               2: returned
+             3: returned
+           4: returned
+         5: returned
+       6: returned
 5040
 */
